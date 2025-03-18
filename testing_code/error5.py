73a959be207a4af9de5b04b88c7919909dc1ef66
Yannis Giamakidis <119029139+yawnggg@users.noreply.github.com>
2025-03-17 23:03:31 -0800

    update

--- testing_code/error5.py
+++
@@ -1,4 +0,0 @@
-# This file contains an invalid if/elif/else statement (else without an if).
-a = 3
-else:
-    a = 6
--- testing_code/error7.py
+++
@@ -1,6 +0,0 @@
-# This file contains an invalid while loop (no while condition).
-a = 1
-i = 0
-while:
-    a = a * 2
-    i = i + 1
